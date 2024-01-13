6495bdc81cf67dda2d557c96f8eccbac22f30a59
Brico97442 <amanvilledamien@hotmail.fr>
2024-01-14 01:57:05 +0400

    create Banner composant

---
+++ src/pages/home/App.jsx
@@ -0,0 +1,10 @@
+import Header from '../../components/Header';
+// import Banner from '../../components/Banner.js';
+import './App.css';
+
+function App() {
+  return <Header/>
+ 
+}
+
+export default App;
