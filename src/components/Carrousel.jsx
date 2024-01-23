e0afd721d34bce466c48f0e144af5a49f11e9ce0
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-01-23 18:36:39 +0400

    stylised img slider scss

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -3,17 +3,21 @@ import logements from "../logements.json"
 import leftArrow from "../assets/Left_arrow.png"
 import rightArrow from "../assets/Right_arrow.png"
 function Carrousel() {
-    const items = logements[0].pictures[2]
+    const items = logements[2].pictures[3]
 
   return (
     <div className="carrousel">
+      
       <button className='carrousel_btn_left'>
       <img src={leftArrow} alt='leftArrow' className='left_arrow_img'/>
       </button>
+      
       <img src={items} alt="img-banner " className="img-banner" />
+      
       <button className='carrousel_btn_right'>
       <img src={rightArrow} alt='rightArrow' className='right_arrow_img'/>
       </button>
+      
     </div>
   );
 }
