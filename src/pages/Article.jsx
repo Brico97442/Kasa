91eaae21e6bcddea64d9e4e5062e2aa2b1d46b9c
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-01-24 17:25:05 +0400

    change width of carrousel for responsive'

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Carrousel from "../components/Carrousel";
+import Collapse from "../components/Collapse";
 
 
 function Article () {
@@ -9,6 +10,10 @@ function Article () {
     <div className="app-container">
     <Header />
     <Carrousel/>
+    <div className="collapse_container">
+      <Collapse/>
+      <Collapse/>
+    </div>
     <Footer />
     </div>
   )
