731405ee9405123ed7cfb85366ad6c3ceefa67b4
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-02-18 13:14:03 +0400

    add media querries for mobile

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,13 +7,15 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <div className="app-container">
-      <Header />
-      <div className="not_found">
-        <h1>404</h1>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
-        <Link className="not_found_link" to="/">
-          Retourner sur la page d'accueil
-        </Link>
+      <div className="not-found">
+        <Header />
+        <div className="not-found-content">
+          <h1>404</h1>
+          <p>Oups! La page que vous demandez n'existe pas.</p>
+          <Link className="not_found_link" to="/">
+            Retourner sur la page d'accueil
+          </Link>
+        </div>
       </div>
       <Footer />
     </div>
